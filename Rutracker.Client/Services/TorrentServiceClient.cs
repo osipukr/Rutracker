c6377e28157478d699e136f6c073453b04f19852
Raman Asipuk <raman_asipuk@epam.com>
2019-05-11 14:41:26 +0300

    added sorting torrents

--- Rutracker.Client/Services/TorrentServiceClient.cs
+++ Rutracker.Client/Services/TorrentServiceClient.cs
@@ -7,29 +7,32 @@ namespace Rutracker.Client.Services
 {
     public class TorrentServiceClient
     {
-        private readonly HttpClient _client;
+        private readonly HttpClient _httpClient;
 
-        private const string _baseUrl = "http://localhost:60744/api/torrents";
-        private const string _torrentsTemplate = _baseUrl + "?search={0}&sort={1}&order={2}&page={3}";
-        private const string _torrentDetailsTemplate = _baseUrl + "/details?torrentid={0}";
+        private readonly string _torrentsTemplate;
+        private readonly string _torrentDetailsTemplate;
 
-        public TorrentServiceClient(HttpClient client)
+        public TorrentServiceClient(HttpClient client, IUriHelper uriHelper)
         {
-            _client = client;
+            _httpClient = client;
+
+            var baseUrl = uriHelper.GetBaseUri() + "api/torrents";
+            _torrentsTemplate = baseUrl + "?search={0}&sort={1}&order={2}&page={3}";
+            _torrentDetailsTemplate = baseUrl + "/details?torrentid={0}";
         }
 
         public async Task<TorrentsViewModel> GetTorrentsAsync(string search, string sort, string order, int page)
         {
-            var requestUrl = string.Format(_torrentsTemplate, search, sort, order, page);
+            var requestUri = string.Format(_torrentsTemplate, search, sort, order, page);
 
-            return await _client.GetJsonAsync<TorrentsViewModel>(requestUrl);
+            return await _httpClient.GetJsonAsync<TorrentsViewModel>(requestUri);
         }
 
         public async Task<TorrentDetailsViewModel> GetTorrentDetailsAsync(long torrentid)
         {
-            var requestUrl = string.Format(_torrentDetailsTemplate, torrentid);
+            var requestUri = string.Format(_torrentDetailsTemplate, torrentid);
 
-            return await _client.GetJsonAsync<TorrentDetailsViewModel>(requestUrl);
+            return await _httpClient.GetJsonAsync<TorrentDetailsViewModel>(requestUri);
         }
     }
 }
