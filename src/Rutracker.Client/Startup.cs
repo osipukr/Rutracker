6508b9dd242b6e3946b76466ee96a0993a34bc7f
osipukr <fredstone624@gmail.com>
2019-07-14 17:15:20 +0300

    added matblazor

--- src/Rutracker.Client/Startup.cs
+++ src/Rutracker.Client/Startup.cs
@@ -1,7 +1,10 @@
+using MatBlazor;
+using Microsoft.AspNetCore.Blazor.Http;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Rutracker.Client.Interfaces;
 using Rutracker.Client.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace Rutracker.Client
 {
@@ -14,10 +17,24 @@ namespace Rutracker.Client
             services.AddSingleton(clientSettings.ApiUriSettings);
             services.AddSingleton(clientSettings.ViewSettings);
             services.AddSingleton<ITorrentsClientService, TorrentsClientService>();
+
+            services.AddLoadingBar();
+            services.AddMatToaster(config =>
+            {
+                config.Position = MatToastPosition.BottomRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = true;
+                config.ShowCloseButton = true;
+                config.MaximumOpacity = 95;
+                config.VisibleStateDuration = 3000;
+            });
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
+            WebAssemblyHttpMessageHandler.DefaultCredentials = FetchCredentialsOption.Include;
+
+            app.UseLoadingBar();
             app.AddComponent<App>("app");
         }
     }
