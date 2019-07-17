2c8a003bb82dd1114a69866e04db0d416bd488e1
FredSt0ne <fredstone624@gmail.com>
2019-07-17 12:44:16 +0300

    added base client service

---
+++ src/Rutracker.Client/Services/BaseClientService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Rutracker.Client.Services
+{
+    public abstract class BaseClientService
+    {
+        protected readonly HttpClient _httpClient;
+
+        protected BaseClientService(HttpClient httpClient) => _httpClient = httpClient;
+
+        protected async Task<TResult> GetJsonAsync<TResult>(string url)
+        {
+            using var response = await _httpClient.GetAsync(url);
+
+            if (response == null)
+            {
+                throw new ArgumentNullException(nameof(response));
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(result);
+            }
+
+            return DeserializeJson<TResult>(result);
+        }
+
+        protected async Task<TResult> PostJsonAsync<TResult>(string url, object jsonObject)
+        {
+            using var content = new StringContent(
+                JsonConvert.SerializeObject(jsonObject),
+                Encoding.UTF8,
+                "application/json");
+
+            using var response = await _httpClient.PostAsync(url, content);
+
+            if (response == null)
+            {
+                throw new ArgumentNullException(nameof(response));
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(result);
+            }
+
+            return DeserializeJson<TResult>(result);
+        }
+
+        private static TResult DeserializeJson<TResult>(string json) => JsonConvert.DeserializeObject<TResult>(json);
+    }
+}
--- src/Rutracker.Client/Services/TorrentsClientService.cs
+++ src/Rutracker.Client/Services/TorrentsClientService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using Rutracker.Client.Interfaces;
 using Rutracker.Client.Settings;
 using Rutracker.Shared.ViewModels.Shared;
@@ -9,14 +8,13 @@ using Rutracker.Shared.ViewModels.Torrents;
 
 namespace Rutracker.Client.Services
 {
-    public class TorrentsClientService : ITorrentsClientService
+    public class TorrentsClientService : BaseClientService, ITorrentsClientService
     {
         private readonly ApiUriSettings _apiUris;
-        private readonly HttpClient _httpClient;
 
         public TorrentsClientService(HttpClient httpClient, ApiUriSettings apiUris)
+            : base(httpClient)
         {
-            _httpClient = httpClient;
             _apiUris = apiUris;
         }
 
@@ -24,21 +22,21 @@ namespace Rutracker.Client.Services
         {
             var requestUri = string.Format(_apiUris.TorrentsIndex, page, pageSize);
 
-            return await _httpClient.PostJsonAsync<TorrentsIndexViewModel>(requestUri, filter);
+            return await PostJsonAsync<TorrentsIndexViewModel>(requestUri, filter);
         }
 
         public async Task<TorrentIndexViewModel> GetTorrentIndexAsync(long id)
         {
             var requestUri = string.Format(_apiUris.TorrentIndex, id);
 
-            return await _httpClient.GetJsonAsync<TorrentIndexViewModel>(requestUri);
+            return await GetJsonAsync<TorrentIndexViewModel>(requestUri);
         }
 
         public async Task<FacetViewModel<string>> GetTitlesAsync(int count)
         {
             var requestUri = string.Format(_apiUris.Titles, count);
 
-            return await _httpClient.GetJsonAsync<FacetViewModel<string>>(requestUri);
+            return await GetJsonAsync<FacetViewModel<string>>(requestUri);
         }
     }
 }
