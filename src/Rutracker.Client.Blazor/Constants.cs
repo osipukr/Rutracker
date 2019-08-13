b699b1b413814ea8f610f5aab6a8c0822a12853d
osipukr <fredstone624@gmail.com>
2019-08-13 19:49:41 +0300

    added constants path

---
+++ src/Rutracker.Client.Blazor/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace Rutracker.Client.Blazor
+{
+    public static class Constants
+    {
+        public static class Path
+        {
+            public const string Home = "/";
+            public const string Torrents = "/torrents";
+            public const string Torrent = "/torrents/{0}";
+            public const string Users = "/users";
+            public const string Contributors = "/contributors";
+
+            public const string Login = "/account/login";
+            public const string Register = "/account/register";
+            public const string Profile = "/account/profile";
+
+            public const string Repository = "https://github.com/osipukr/Rutracker-Blazor";
+        }
+    }
+}
