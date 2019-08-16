37180592518afd147c46ec65fb132772436ca0d6
FredSt0ne <fredstone624@gmail.com>
2019-08-16 13:08:57 +0300

    added resources for login and register models

--- src/Rutracker.Shared.Models/ViewModels/Account/RegisterViewModel.cs
+++ src/Rutracker.Shared.Models/ViewModels/Account/RegisterViewModel.cs
@@ -1,24 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Rutracker.Shared.Models.Resources;
 
 namespace Rutracker.Shared.Models.ViewModels.Account
 {
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = nameof(RegisterViewModelResource.UserNameDisplayName), ResourceType = typeof(RegisterViewModelResource))]
+        [StringLength(maximumLength: 100, MinimumLength = 5,
+            ErrorMessageResourceName = nameof(RegisterViewModelResource.UserNameErrorMessage),
+            ErrorMessageResourceType = typeof(RegisterViewModelResource))]
         public string UserName { get; set; }
 
         [Required]
-        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessageResourceName = nameof(RegisterViewModelResource.EmailErrorMessage), 
+            ErrorMessageResourceType = typeof(RegisterViewModelResource))]
+        [Display(Name = nameof(RegisterViewModelResource.EmailDisplayName),
+            ResourceType = typeof(RegisterViewModelResource))]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [MinLength(6)]
+        [Display(Name = nameof(RegisterViewModelResource.PasswordDisplayName), ResourceType = typeof(RegisterViewModelResource))]
+        [StringLength(maximumLength: 100, MinimumLength = 6,
+            ErrorMessageResourceName = nameof(RegisterViewModelResource.PasswordErrorMessage),
+            ErrorMessageResourceType = typeof(RegisterViewModelResource))]
         public string Password { get; set; }
 
         [Required]
-        [DataType(DataType.Password)]
-        [Compare(nameof(Password))]
+        [Display(Name = nameof(RegisterViewModelResource.ConfirmPasswordDisplayName), ResourceType = typeof(RegisterViewModelResource))]
+        [Compare(nameof(Password),
+            ErrorMessageResourceName = nameof(RegisterViewModelResource.ConfirmPasswordErrorMessage),
+            ErrorMessageResourceType = typeof(RegisterViewModelResource))]
         public string ConfirmPassword { get; set; }
     }
 }
